4c48d2a686bd713ce743ec0a0e0964b11906b571
Žiga Holc <ziga.holc8@gmail.com>
2022-07-21 20:56:50 +0200

    added pagination in CardList.tsx

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,25 +2,19 @@ import { useState, useEffect } from 'react'
 import { Card } from '../components/Card'
 import { useFetch } from '../hooks/useFetch'
 
-type NftProps = {
-  id: number
-  img: {
-    id: string
-    url: string
-  }
-  name: string
-  price: number
-}
-
 export function CardList() {
   const { loading, data } = useFetch()
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(0)
   const [apes, setApes] = useState(Array<any>)
 
   useEffect(() => {
     if (loading) return
     setApes(data[page])
-  }, [loading])
+  }, [loading, page])
+
+  const handlePage = (index: number):void => {
+    setPage(index)
+  }
 
   return (
     <div className="container mx-auto">
@@ -29,6 +23,13 @@ export function CardList() {
           <Card key={ape.id} {...ape} />
         ))}
       </div>
+      <div className="flex justify-center mt-5">
+        {!loading && <div>
+          {data.map((_,index) => {
+            return (<button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l" key={index} onClick={() => handlePage(index)}>{index}</button>)
+          })}
+        </div>}
+      </div>
     </div>
-  )
+  ) 
 }
